4baf5cee89fb3cd72c229bdb8a1c2511235d5901
simsimjae <simsimjae@naver.com>
2020-01-05 22:42:43 +0900

    Updates

---
+++ component---src-pages-index-js-1c54a57c4549301d3bc3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RXBc:function(e,a,t){"use strict";t.r(a),t.d(a,"PostCardDatas",(function(){return c}));var n=t("q1tI"),r=t.n(n),l=t("Bl7J"),s=t("vrFN"),o=t("xaIn"),c=(t("sg+I"),"758884137");a.default=function(e){var a=e.data,t=a.allMarkdownRemark.edges,n=a.allFile.edges;return r.a.createElement(l.a,null,r.a.createElement(s.a,{title:"home"}),r.a.createElement(o.a,{posts:t,images:n}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-1c54a57c4549301d3bc3.js.map
