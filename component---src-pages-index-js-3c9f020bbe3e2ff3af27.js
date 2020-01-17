6424f120902bedd7b470b6ab388bd291a2507374
simsimjae <191429@ssg.com>
2020-01-17 15:51:26 +0900

    Updates

--- component---src-pages-index-js-3c9f020bbe3e2ff3af27.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RXBc:function(e,a,t){"use strict";t.r(a),t.d(a,"PostCardDatas",(function(){return c}));var n=t("q1tI"),r=t.n(n),l=t("Bl7J"),o=t("vrFN"),s=t("xaIn"),c="758884137";a.default=function(e){var a=e.data;console.log(a);var t=a.allMarkdownRemark.edges,n=a.allFile.edges;return r.a.createElement(l.a,null,r.a.createElement(o.a,{title:"home"}),r.a.createElement(s.a,{posts:t,images:n}))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-3c9f020bbe3e2ff3af27.js.map
